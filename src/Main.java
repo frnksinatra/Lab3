cc0449803a0a5bb58c1d41c6f10fec0cfaa69fa1
frnksinatra <ferasbaraka0@gmail.com>
2017-06-29 00:16:48 -0400

    have a formatted and working code

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,55 @@
 import java.util.Scanner;
 
 /**
- * promt the user to enter an integer
+ * prompt the user to enter an integer
  * display a table of squares and cubes from 1 to the value entered.
  * ask if the user wants to continue
  * assume that the user will enter valid data.
  * only continue if the user agrees to
- *
- *
+ * <p>
+ * <p>
  * make
  */
 
 
-
 public class Main {
+
+
+
 
     public static void main(String[] args) {
 
+        String userContinue = "y";
         int userInput;
 
-        System.out.println("Learn your squares and cubes!");
-        System.out.println();
-        System.out.println("Enter an integer:");
-        Scanner scan = new Scanner(System.in);
-            if (scan.hasNextInt()) {
-                userInput = scan.nextInt();
-            }else {
-                System.out.println("That isn't a number!");
-            }
-        for (int i = 1; i < userInput ; i++) {
-            System.out.println((int) Math.pow(i,2));
+        while (userContinue.equalsIgnoreCase("y")) {
 
+
+            System.out.println("Learn your squares and cubes!");
+            System.out.println();
+            System.out.println("Enter an integer:");
+
+
+            Scanner scan = new Scanner(System.in);
+
+
+            while (!scan.hasNextInt()) {
+                System.out.println("That isn't a number!");
+                scan.nextLine();
+            }
+            userInput = scan.nextInt();
+
+
+            System.out.printf("%s%16s%16s\n", "x", "x^2", "x^3");
+            System.out.println("=====================================");
+
+            for (int i = 1; i <= userInput; i++) {
+
+                System.out.printf("%d%16d%16d\n", i, (i + ((int) Math.pow(i, 2))), ((int) Math.pow(i, 3)));
+            }
+            System.out.println("Want to use a different number? (y/n)");
+            userContinue = scan.next();
         }
+        System.out.println("goodbye!");
     }
 }
